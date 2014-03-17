46d982f1da5cd348f392bf9ee91e90de40e7f61b
Geniusun <156289565@qq.com>
2014-03-17 17:36:00 +0800

    some animation

--- studio/script/timeline.js
+++ studio/script/timeline.js
@@ -1,15 +1,40 @@
 (function($) {
 
+  $(function(){  
+      $('#logo_main').fadeIn(1000,title);
+      console.log('xxxx');
+  });
+
+	var title=function(){
+
+		$('#title_text').delay(800).fadeIn(2000,slogan);
+		console.log('??');};
+
+	var slogan=function(){
+
+		$('#slogan').delay(800).fadeIn(2000);
+		console.log('??');};
+
+
+
+
+
   $(function(){
     var light_on = function() {
       $('#logo_light').fadeTo(1000, 1.0, light_off);
     };
   
     var light_off = function() {
-      $('#logo_light').fadeTo(1000, 0.0, light_on);
+      $('#logo_light').fadeTo(1000, 0.5, light_on);
     };
   
     light_off();
   });
 
+
+
+
+
 })($);
+
+
