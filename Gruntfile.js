cfde9d3ba8b3b7bd70474e7aa0ebdce96cf22e06
trinity <imandry.c@gmail.com>
2014-03-21 23:53:36 +0800

    Gruntfile: watch src/asset directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,7 +67,7 @@ module.exports = function(grunt) {
         tasks: ['jshint', 'copy:script'],
       },
       resource: {
-        files: ['asset/**/*'],
+        files: ['src/asset/**/*'],
         tasks: ['copy:resource'],
       },
       document: {
