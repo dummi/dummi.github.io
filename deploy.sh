c85c9d9ba9aa4dfad6b0fd3b658c825acbaedfe6
LAN Xingcan <lanxingcan@gmail.com>
2014-07-01 01:12:52 +0800

    Deploy to master branch

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git push https://github.com/dummi/dummi.github.io.git `git subtree split -P studio`:refs/heads/built
+git push https://github.com/dummi/dummi.github.io.git `git subtree split -P studio`:refs/heads/master
